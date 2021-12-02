eb0a843825b5f94f72513fb0faf73446ae027c09
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-02 06:58:05 +0100

    logging in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
 
 function App() {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [currentUser, setCurrentUser] = useState({});
+
+	useEffect(() => {
+		(async () => {
+		const requestOptions = {
+			method: 'GET',
+			credentials: 'include'
+		};
+			const response = await fetch('http://localhost:3003/currentuser', requestOptions);
+			if (response.ok) {
+				const _currentUser = await response.json();
+				setCurrentUser(prev => ({ ...prev, ..._currentUser }));
+			}
+		})();
+	}, []);
 
 	const handleUsername = (e) => {
 		let _username = e.target.value;
@@ -19,17 +34,22 @@ function App() {
 		e.preventDefault();
 		const requestOptions = {
 			method: 'POST',
+			credentials: "include",
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({ username, password })
 		};
 		const response = await fetch('http://localhost:3003/login', requestOptions);
-		const data = await response.json();
-		console.log(data);
+		if (response.ok) {
+			const _currentUser = await response.json();
+			console.log(_currentUser);
+			setCurrentUser(prev => ({ ...prev, ..._currentUser }));
+		}
 	}
 
 	return (
 		<div className="App">
 			<h1>MERN Showcase App</h1>
+			<h2>Current User: {currentUser.firstName} {currentUser.lastName}</h2>
 			<form>
 				<fieldset>
 					<legend>Login</legend>
@@ -42,7 +62,7 @@ function App() {
 						<input type="password" id="password" value={password} onChange={handlePassword} />
 					</div>
 					<div className="buttonRow">
-						<button onClick={handleButton}>Login</button>
+						<button onClick={handleButton}>Submit</button>
 					</div>
 				</fieldset>
 			</form>
